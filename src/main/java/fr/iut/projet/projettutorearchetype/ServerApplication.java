4410a629c87efab46c30d331dc8eeec4eee3be2d
tintin92350 <rodic.quentin.corporation@gmail.com>
2020-01-22 21:32:54 +0100

    Clean update for start

--- src/main/java/fr/iut/projet/projettutorearchetype/ServerApplication.java
+++ src/main/java/fr/iut/projet/projettutorearchetype/ServerApplication.java
@@ -1,11 +1,10 @@
 package fr.iut.projet.projettutorearchetype;
 
-import org.keycloak.adapters.springboot.KeycloakAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication( exclude = { KeycloakAutoConfiguration.class } )
+@SpringBootApplication
 @Configuration
 public class ServerApplication {
 
--- src/main/java/fr/iut/projet/projettutorearchetype/configurations/BasicSecurityConfiguration.java
+++ src/main/java/fr/iut/projet/projettutorearchetype/configurations/BasicSecurityConfiguration.java
@@ -1,6 +1,5 @@
 package fr.iut.projet.projettutorearchetype.configurations;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
