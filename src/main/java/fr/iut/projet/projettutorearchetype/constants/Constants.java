8e1ad1feb9dfb7e79b34e73cda6a464f8cb77822
Panpan <panpan.thn@gmail.com>
2020-03-27 16:36:33 +0100

    Merged with feature_role (Role.java becomes rolesEnum.java) Adding Constants api (not implemented yet)

---
+++ src/main/java/fr/iut/projet/projettutorearchetype/constants/Constants.java
@@ -0,0 +1,8 @@
+package fr.iut.projet.projettutorearchetype.constants;
+
+public final class Constants {
+
+
+    public static final String apiConstant = "api/";
+
+}
--- src/main/java/fr/iut/projet/projettutorearchetype/models/RolesEnum.java
+++ src/main/java/fr/iut/projet/projettutorearchetype/models/RolesEnum.java
@@ -1,6 +1,8 @@
 package fr.iut.projet.projettutorearchetype.models;
 
 public enum RolesEnum {
+    ADMINISTRATOR,
+    APPRENTICESHIP_MANAGER,
+    DEPARTMENT_MANAGER,
     STUDENT,
-    MANAGER
 }
